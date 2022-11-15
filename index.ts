04a546d6c59a0d918a6f451c7c488ff8a66d104a
Fran Hoey <fran.hoey@intechnica.co.uk>
2022-11-15 12:34:38 +0000

    Initial Setup

---
+++ index.ts
@@ -0,0 +1,32 @@
+/*
+-------------------------------------------
+Intechnica Technical Test - Numbers To Words
+-------------------------------------------
+Implement the 'NumberToWords' function below.
+This function should take a whole number as an argument and return a string representation of the number.
+
+Examples:
+  Input: 0      Output: zero
+  Input: 1      Output: one
+  Input: 23     Output: twenty three
+  Input: 80     Output: eighty
+  Input: 200    Output: two hundred
+  Input: 445    Output: four hundred and forty five
+  Input: 7382   Output: seven thousand three hundred and eighty two
+  
+The function should work for any whole number between 0 and 9999.
+There is no need to worry about how the function works for inputs that are not numbers.
+*/
+
+
+// Function signature
+export default function numberToWord(inputValue: number): string {
+  let result: string = "";
+  
+  //Enter your code here
+
+  return result;
+}
+
+
+console.log(numberToWord(9));
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Requirements
+
+# node version 16.0 or higher
+
+# Commands
+
+# npm install - install dependencies (tsc compiler, typescript, mocha, chai ... )
+
+# tsc index.ts - compile typescript file to javascript file
+
+# node index.js - run index.js file
+
